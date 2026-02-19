cef4dc3c72c75d7f11bf68bf40833a0e3e44ea7d
Steven Sawadisavi <stevesavi+lewagon@gmail.com>
2026-02-19 15:03:02 +0900

    implemented bookmark destroy and fixed list show

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,6 +15,10 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
   end
 
   private
